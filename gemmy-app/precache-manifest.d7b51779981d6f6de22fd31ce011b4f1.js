6d322cd8f6b52faf68b27b14c55980811b5bbc31
Wanchao Zhang <wonderbeyond@gmail.com>
2018-12-20 21:27:17 +0800

    Updates

---
+++ gemmy-app/precache-manifest.d7b51779981d6f6de22fd31ce011b4f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9074c24d34c5c82e40ff",
+    "url": "/static/css/main.bd0e55c3.chunk.css"
+  },
+  {
+    "revision": "9074c24d34c5c82e40ff",
+    "url": "/static/js/main.9074c24d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5fb12d1b8d27b0dba02bcaea1cec3d2e",
+    "url": "/index.html"
+  }
+];
