8b5db9add193b3a3edfdbf8907042a724368d475
Wanchao Zhang <wonderbeyond@gmail.com>
2018-12-20 23:07:03 +0800

    Updates

---
+++ gemmy-app/precache-manifest.8dd4da76d9a2453e49eedf162398ef41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b34d4df87771819bee4e",
+    "url": "https://wonderbeyond.github.io/gemmy/gemmy-app/static/css/main.bd0e55c3.chunk.css"
+  },
+  {
+    "revision": "b34d4df87771819bee4e",
+    "url": "https://wonderbeyond.github.io/gemmy/gemmy-app/static/js/main.b34d4df8.chunk.js"
+  },
+  {
+    "revision": "61513c9de70eb899c0cf",
+    "url": "https://wonderbeyond.github.io/gemmy/gemmy-app/static/js/1.61513c9d.chunk.js"
+  },
+  {
+    "revision": "c0bf04da6db947d0c1b4",
+    "url": "https://wonderbeyond.github.io/gemmy/gemmy-app/static/js/runtime~main.c0bf04da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://wonderbeyond.github.io/gemmy/gemmy-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1ad3819c8bd834790a46cd052c9ded49",
+    "url": "https://wonderbeyond.github.io/gemmy/gemmy-app/index.html"
+  }
+];
--- gemmy-app/service-worker.js
+++ gemmy-app/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c2c77d0f509969c734cf86f75b91b41a.js"
+  "https://wonderbeyond.github.io/gemmy/gemmy-app/precache-manifest.8dd4da76d9a2453e49eedf162398ef41.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("https://wonderbeyond.github.io/gemmy/gemmy-app/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
