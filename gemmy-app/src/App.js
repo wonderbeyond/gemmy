605678d9ab78b16e177e1737f9e3157992c18a48
Wanchao Zhang <wonderbeyond@gmail.com>
2019-03-10 21:02:15 +0800

    update gemmy-app

--- gemmy-app/src/App.js
+++ gemmy-app/src/App.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import logo from './cat.svg';
 import './App.css';
 
-import {GemmyClient, GEM2Html} from './lib/gemmy';
+import snarkdown from 'snarkdown';
+import {GemmyClient} from 'gemmy-client';
 
 class App extends Component {
   constructor(props) {
@@ -12,15 +13,15 @@ class App extends Component {
     }
   }
   async componentDidMount() {
-    let gm = new GemmyClient()
-    let hitGem = await gm.randomGet()
+    let gmc = new GemmyClient()
+    let hitGem = await gmc.randomGet()
     this.setState({hitGem: hitGem});
   }
   render() {
     return (<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo"/>
-        <p dangerouslySetInnerHTML={{__html: GEM2Html(this.state.hitGem) || "喵~"}}>
+        <p dangerouslySetInnerHTML={{__html: snarkdown(this.state.hitGem.content || "Mew~")}}>
         </p>
         <p className="credit-info">
           Powered by <a className="App-link" href="https://github.com/wonderbeyond/gemmy" rel="noopener noreferrer">wonderbeyond/gemmy</a>
